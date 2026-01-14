7c479078b3142d2e977f99f09cd7132d03ae84dd
Allan Silva <allandasilva33@outlook.com>
2026-01-14 02:11:23 -0300

    Criar componente Background e estiliza-lo

---
+++ src/components/bg/Background.tsx
@@ -0,0 +1,11 @@
+import styles from "./Background.module.css";
+
+const Background = () => {
+  return (
+    <div className={styles.background}>
+      <div className={styles.backgroundRadial} />
+    </div>
+  );
+};
+
+export default Background;
