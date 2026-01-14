617993adf2f412f7147e4056547e7652bf1f1bb6
Allan Silva <allandasilva33@outlook.com>
2026-01-14 04:01:31 -0300

    Adicionar teste unitário para o componente title

--- src/components/title/Title.test.tsx
+++ src/components/title/Title.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import CountButton from "./CountButton";
+import Title from "./Title";
 import "@testing-library/jest-dom/vitest";
 
 describe("CountButton Component", () => {
   it("the value should come from the children", () => {
-    render(<CountButton>Increment</CountButton>);
-    const element = screen.getByRole("button", { name: "Increment" });
+    render(<Title>Counter App</Title>);
+    const element = screen.getByText("Counter App");
     expect(element).toBeInTheDocument();
+    expect(element).toHaveRole("heading");
   });
 });
